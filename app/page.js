55ba81d499f10f69ec674e94597e3c2aa671e2e6
Hh440 <harsh100045@gmail.com>
2024-08-30 18:58:50 +0530

    initial commit

---
+++ app/page.js
@@ -0,0 +1,20 @@
+
+
+import dynamic from 'next/dynamic'
+
+
+const Scene = dynamic(() => import('./components/Scene'), {
+
+    ssr: false,
+
+})
+
+
+
+export default function Home() {
+  return (
+    <main className="relative h-screen">
+      <Scene/>
+    </main>
+  )
+}
